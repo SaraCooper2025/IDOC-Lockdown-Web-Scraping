47f66beb1e24a58f00ee315fdc2e55304b8a019c
SaraCooper2025 <saracooper2025@u.northwestern.edu>
2025-08-27 21:51:07 -0500

    Add files via upload

---
+++ IDOC_Lockdown_Web_Scraping.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from playwright.async_api import async_playwright
+import asyncio
+import time
+import csv
+from datetime import datetime, date
+import pytz
+from datetime import timedelta
+import csv
+
+key_phrase = "on lockdown"
+cst_timezone = pytz.timezone('US/Central')
+now_cst = datetime.now(cst_timezone)
+current_date = now_cst.date()
+yesterday_date = (current_date)-timedelta(days=1)
+
+if now_cst.hour >= 23 or now_cst.hour < 7:
+    shift = 'Shift 1'
+elif now_cst.hour >= 7 and now_cst.hour < 15:
+    shift = 'Shift 2'
+else:
+    shift = 'Shift 3'
+
+urls = [
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.big-muddy-river-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.centralia-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.danville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.decatur-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.dixon-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.east-moline-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.graham-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.hill-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.illinois-river-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.jacksonville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.lawrence-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.lincoln-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.logan-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.menard-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.pinckneyville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.pontiac-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.robinson-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.shawnee-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.sheridan-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.southwestern-illinois-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.stateville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.taylorville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.vandalia-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.vienna-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.western-illinois-correctional-center.html"
+]
+
+sites = [
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.graham-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.centralia-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.lawrence-correctional-center.html",
+]
+
+async def main():
+    results = []
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        for url in urls:
+            try:
+                await page.goto(url)
+                time.sleep(2)
+                content = await page.content()
+                found = key_phrase.lower() in content.lower()
+                facility_name = url.split('https://idoc.illinois.gov/facilities/lockdowninformation/facility.')[1].split('.html')[0].replace('-', ' ')
+                if now_cst.hour >= 23 or now_cst.hour < 7:
+                    shift = 'Shift 1'
+                elif now_cst.hour >= 7 and now_cst.hour < 15:
+                    shift = 'Shift 2'
+                else:
+                    shift = 'Shift 3'
+                results.append({"URL": facility_name, "Contains Key Phrase": found, "Date": current_date, "Shift": shift})
+            except Exception as e:
+                results.append({"URL": facility_name, "Contains Key Phrase": False, "Error": str(e)})
+        await browser.close()
+    df = pd.DataFrame(results)
+    print(df)
+    return df # Return the DataFrame
+asyncio.run(main())
