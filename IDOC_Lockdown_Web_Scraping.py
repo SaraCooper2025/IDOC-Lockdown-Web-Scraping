43ae3db611914fa748ee0bf5e47b13eb66057307
SaraCooper2025 <saracooper2025@u.northwestern.edu>
2025-08-29 15:14:17 -0500

    updates

--- IDOC_Lockdown_Web_Scraping.py
+++ IDOC_Lockdown_Web_Scraping.py
@@ -7,8 +7,10 @@ from datetime import datetime, date
 import pytz
 from datetime import timedelta
 import csv
+import os
 
-key_phrase = "on lockdown"
+key_phrase1 = "No visits"
+key_phrase2 = "limited visits"
 cst_timezone = pytz.timezone('US/Central')
 now_cst = datetime.now(cst_timezone)
 current_date = now_cst.date()
@@ -40,6 +42,7 @@ urls = [
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.logan-correctional-center.html",
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.menard-correctional-center.html",
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.pinckneyville-correctional-center.html",
+    "https://idoc.illinois.gov/facilities/lockdowninformation/facility.joliet-treatment-center.html"
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.pontiac-correctional-center.html",
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.robinson-correctional-center.html",
     "https://idoc.illinois.gov/facilities/lockdowninformation/facility.shawnee-correctional-center.html",
@@ -63,22 +66,23 @@ async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch()
         page = await browser.new_page()
-        for site in sites:
+        for url in urls:
             try:
-                await page.goto(site)
+                await page.goto(url)
                 time.sleep(2)
                 content = await page.content()
-                found = key_phrase.lower() in content.lower()
-                facility_name = site.split('https://idoc.illinois.gov/facilities/lockdowninformation/facility.')[1].split('.html')[0].replace('-', ' ')
+                full_lockdown = int(key_phrase1.lower() in content.lower())
+                partial_lockdown = int(key_phrase2.lower()in content.lower())
+                facility_name = url.split('https://idoc.illinois.gov/facilities/lockdowninformation/facility.')[1].split('.html')[0].replace('-', ' ')
                 if now_cst.hour >= 23 or now_cst.hour < 7:
                     shift = 'Shift 1'
                 elif now_cst.hour >= 7 and now_cst.hour < 15:
                     shift = 'Shift 2'
                 else:
                     shift = 'Shift 3'
-                results.append({"URL": facility_name, "Contains Key Phrase": found, "Date": current_date, "Shift": shift})
+                results.append({"URL": facility_name, "Full Lockdown": full_lockdown, "Partial Lockdown": partial_lockdown, "Date": current_date, "Time": now_cst, "Shift": shift})
             except Exception as e:
-                results.append({"URL": facility_name, "Contains Key Phrase": False, "Error": str(e)})
+                results.append({"URL": facility_name, "Full Lockdown": False, "Partial Lockdown": False, "Error": str(e)})
         await browser.close()
     df = pd.DataFrame(results)
     print(df)
@@ -87,3 +91,13 @@ asyncio.run(main())
 
 lockdown_data = asyncio.run(main())
 lockdown_data.to_csv(f'{current_date_str}_{current_hour_str}_{shift}_record.csv', index =False)
+
+master_file = "master_lockdown.csv"
+
+if os.path.exists(master_file):
+    master_df = pd.read_csv(master_file)
+    master_df = pd.concat([master_df,lockdown_data], ignore_index=True)
+else:
+    master_df = lockdown_data
+
+master_df.to_csv(master_file, index=False)
---
+++ master_tracker.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import requests
+
+df = pd.read_csv('master_lockdown.csv', encoding='latin1')
+
+print(df)
+
+
